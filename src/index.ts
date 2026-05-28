853b7d61ed1b88bd7cbabda1857476400182afa0
JPasache <jcpg1982@hotmail.com>
2026-05-28 00:30:03 -0500

    fix(policy): add child safety standards page and server route

--- src/index.ts
+++ src/index.ts
@@ -52,6 +52,10 @@ app.get('/terms', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
+app.get('/safety', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
 // Setup Signaling
 setupSignaling(io);
 
